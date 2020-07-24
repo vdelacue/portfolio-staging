43960d6fd922ca2900f5779335067c3e907a70ee
Vanessa de La Cuetara <vdelacuetara@gmail.com>
2020-07-24 14:07:02 -0400

    Styles header adds theme

---
+++ src/components/Headers/IndexHeader.js
@@ -0,0 +1,61 @@
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+// core components
+
+function IndexHeader() {
+  let pageHeader = React.createRef();
+
+  React.useEffect(() => {
+    if (window.innerWidth > 991) {
+      const updateScroll = () => {
+        let windowScrollTop = window.pageYOffset / 3;
+        pageHeader.current.style.transform =
+          "translate3d(0," + windowScrollTop + "px,0)";
+      };
+      window.addEventListener("scroll", updateScroll);
+      return function cleanup() {
+        window.removeEventListener("scroll", updateScroll);
+      };
+    }
+  });
+
+  return (
+    <>
+      <div className="page-header clear-filter" filter-color="black">
+
+        <div
+          className="page-header-image"
+          style={{
+            backgroundImage: "url(" + require("assets/img/header-bg-rp3600.png") + ")",
+          }}
+          ref={pageHeader}
+        ></div>
+        <Container>
+          <div className="content-center brand">
+            {/*<img*/}
+            {/*  alt="..."*/}
+            {/*  className="n-logo"*/}
+            {/*  src={require("assets/img/now-logo.png")}*/}
+            {/*></img>*/}
+            <h1 className="h1-seo">Vanessa de La Cuetara</h1>
+            <h3>Full-Stack Engineer</h3>
+          </div>
+          <h6 className="category category-absolute">
+            Design.
+          </h6>
+          <h6 className="category category-absolute">
+            Create.
+          </h6>
+          <h6 className="category category-absolute">
+            Disrupt.
+          </h6>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
---
+++ src/views/index-sections/Images.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+// reactstrap components
+import { Container, Row, Col } from "reactstrap";
+
+// core components
+
+function Images() {
+  return (
+    <>
+      <div className="section section-images">
+        <Container>
+          <Row>
+            <Col md="12">
+              <div className="hero-images-container">
+                <img
+                  alt="..."
+                  src={require("assets/img/hero-image-1a.png")}
+                ></img>
+              </div>
+              <div className="hero-images-container-1">
+                <img
+                  alt="..."
+                  src={require("assets/img/hero-image-2a.png")}
+                ></img>
+              </div>
+              <div className="hero-images-container-2">
+                <img
+                  alt="..."
+                  src={require("assets/img/hero-image-3a.png")}
+                ></img>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default Images;
